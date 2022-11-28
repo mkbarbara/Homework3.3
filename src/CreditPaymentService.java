01470c0ed224f813e654244dc41319f5207d202a
Barbara Makarova <makarena2001barbara@yandex.ru>
2022-11-28 14:07:13 +0300

    change for homework

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
 
     public int calculate(int months, int amount, double loan) {
-        double monthLoan = loan/12/100;
-        double ann = (monthLoan*Math.pow(1+monthLoan, months))/(Math.pow(1+monthLoan, months)-1);
-        double result = amount*ann;
+        double monthLoan = loan / 12 / 100;
+        double ann = (monthLoan * Math.pow(1 + monthLoan, months)) / (Math.pow(1 + monthLoan, months) - 1);
+        double result = amount * ann;
         return (int) result;
     }
 }
